4cc548b6e73812f8026a76ef3946c4dcbee52a29
netdong-li <east2050@gmail.com>
2024-02-29 13:11:21 +0800

    Health endpoint

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/ping", (req, res) => {
-  res.send({ message: "pong" });
+app.get("/", (req, res) => {
+  res.send({ status: "ok" });
 });
 
 app.listen(port, () => {
