ae9749cbd0cf3cdfea9d570bd00249845bce9ea9
netdong-li <east2050@gmail.com>
2024-02-29 17:53:31 +0800

    Add model: book

---
+++ src/model/book.ts
@@ -0,0 +1,12 @@
+// Book represents the structure of a book
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  published_at: string;
+  description: string;
+  isbn: string;
+  total_pages: number;
+  created_at: Date;
+  updated_at: Date;
+}
